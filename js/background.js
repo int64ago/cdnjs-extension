83d4804b52e767b997723d996d022405459603f6
Cody Chan <int64ago@gmail.com>
2015-12-31 00:55:39 +0800

    Open options.html on installed && Update options.html

--- js/background.js
+++ js/background.js
@@ -1,5 +1,10 @@
 (function() {
-	
+
+    chrome.runtime.onInstalled.addListener(function() {
+        chrome.tabs.create({
+            url: "options.html"
+        });
+    });
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if (request.method == "qiniu") {
             chrome.storage.sync.get({
