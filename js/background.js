47c9a014fff7f6a7d48925bc6d4839ae3322dc63
Cody Chan <int64ago@gmail.com>
2016-01-31 12:36:06 +0800

    Don't open options onInstalled

--- js/background.js
+++ js/background.js
@@ -1,10 +1,5 @@
 (function() {
 
-    chrome.runtime.onInstalled.addListener(function() {
-        chrome.tabs.create({
-            url: "options.html"
-        });
-    });
     chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
         if (request.method == "info") {
             chrome.storage.sync.get({
